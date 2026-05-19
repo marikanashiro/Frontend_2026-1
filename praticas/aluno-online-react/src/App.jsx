357d48b32faa523449b97e029bfb2b2ba704e722
Mariana Kanashiro <m.kanashiro@iesb.edu.br>
2026-05-19 16:50:59 -0300

    feat: conclui pratica05

--- praticas/aluno-online-react/src/App.jsx
+++ praticas/aluno-online-react/src/App.jsx
@@ -1,14 +1,14 @@
 import "./App.css";
-import Dashboard from "../pages/Dashboard";
-import Faltas from "../pages/Faltas";
-import Notas from "../pages/Notas";
-import Boletos from "../pages/Boletos";
-import Requerimentos from "../pages/Requerimentos";
+import Dashboard from "./pages/Dashboard";
+import Faltas from "./pages/Faltas";
+import Notas from "./pages/Notas";
+import Boletos from "./pages/Boletos";
+import Requerimentos from "./pages/Requerimentos";
 import Login from "./pages/Login";
 
 function App() {
   return (
-    <Notas/>
+    <Login/>
   );
 }
 
