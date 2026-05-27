07488bea84da8acc0543dee4057844cec1fa5763
Mariana Kanashiro <m.kanashiro@iesb.edu.br>
2026-05-27 17:19:04 -0300

    feat: conclui pratica07. Fecha #12

--- praticas/aluno-online-react/src/contexts/AuthContext.jsx
+++ praticas/aluno-online-react/src/contexts/AuthContext.jsx
@@ -1,34 +1,3 @@
-import { createContext, useEffect, useState } from "react";
+import { createContext } from "react";
 
 export const AuthContext = createContext();
-
-export function AuthProvider({ children }) {
-    const [autenticado, setAutenticado] = useState(false);
-    const [usuario, setUsuario] = useState(null);
-
-    useEffect(() => {
-        const usuarioSalvo = localStorage.getItem("usuario");
-        if (usuarioSalvo) {
-            setUsuario(JSON.parse(usuarioSalvo));
-            setAutenticado(true);
-        }
-    }, []);
-
-    const login = (dadosUsuario) => {
-        setUsuario(dadosUsuario);
-        setAutenticado(true);
-        localStorage.setItem("usuario", JSON.stringify(dadosUsuario));
-    };
-
-    const logout = () => {
-        setUsuario(null);
-        setAutenticado(false);
-        localStorage.removeItem("usuario");
-    };
-
-    return (
-        <AuthContext.Provider value={{ autenticado, usuario, login, logout }}>
-            {children}
-        </AuthContext.Provider>
-    );
-}
--- praticas/aluno-online-react/src/main.jsx
+++ praticas/aluno-online-react/src/main.jsx
@@ -3,7 +3,7 @@ import { createRoot } from "react-dom/client";
 import "./index.css";
 import App from "./App.jsx";
 import { BrowserRouter } from "react-router-dom";
-import { AuthProvider } from "./contexts/AuthContext.jsx";
+import { AuthProvider } from "./contexts/AuthProvider.jsx";
 
 createRoot(document.getElementById("root")).render(
   <StrictMode>
